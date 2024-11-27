a0ab3b3abbe283985279e92abd79a4ce50c17394
sumit <sumitsahu587@yahoo.in>
2024-11-27 10:12:25 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: TasksComponent },
+    // { path: 'about', component: AboutComponent },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));
